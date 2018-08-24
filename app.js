45bec3cf4b963effeb7dbb0e0f6667c769948b4a
TangentialDevelopment <joetang148@gmail.com>
2018-08-24 17:20:15 -0400

    added RESTFUL routing

--- app.js
+++ app.js
@@ -1,37 +1,83 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express    = require("express"),
+    app        = express(),
+    bodyParser = require("body-parser"),
+    mongoose   = require("mongoose");
 
-var campgrounds = [
-  {name: "Salmon Creek", image: "https://images.unsplash.com/photo-1515408320194-59643816c5b2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=fcbebfe204ad7e04d558d7e0cbc0d2eb&auto=format&fit=crop&w=500&q=60"},
-  {name: "Granite Hill", image: "https://images.unsplash.com/photo-1525209149972-1d3faa797c3c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=053f91dd9aee1cc7bc5cafca28cb625c&auto=format&fit=crop&w=500&q=60"},
-  {name: "Stoney Creek", image: "https://images.unsplash.com/photo-1533597818151-d1071f26fe32?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d801e2dda51c0d284878778edf9172a1&auto=format&fit=crop&w=500&q=60"}
-]
-
-
+mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
+
+// schema
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+});
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//   {
+//     name: "Salmon Creek",
+//     image: "https://images.unsplash.com/photo-1515408320194-59643816c5b2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=fcbebfe204ad7e04d558d7e0cbc0d2eb&auto=format&fit=crop&w=500&q=60",
+//     description: "large granite hill, no bathrooms, no water. just rock"
+//   }, function(err, campground) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log(campground);
+//     }
+//   });
+
+// var campgrounds = [
+//   {name: "Salmon Creek", image: "https://images.unsplash.com/photo-1515408320194-59643816c5b2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=fcbebfe204ad7e04d558d7e0cbc0d2eb&auto=format&fit=crop&w=500&q=60"},
+//   {name: "Granite Hill", image: "https://images.unsplash.com/photo-1525209149972-1d3faa797c3c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=053f91dd9aee1cc7bc5cafca28cb625c&auto=format&fit=crop&w=500&q=60"},
+//   {name: "Stoney Creek", image: "https://images.unsplash.com/photo-1533597818151-d1071f26fe32?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d801e2dda51c0d284878778edf9172a1&auto=format&fit=crop&w=500&q=60"}
+// ]
 
 app.get("/", function(req, res) {
    res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res) {
-  res.render("campgrounds", {campgrounds:campgrounds});
-});
-
-app.get("/campgrounds/new", function(req, res) {
-  res.render("new.ejs")
+  Campground.find({},function(err, allCampgrounds){
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", {campgrounds:allCampgrounds});
+    }
+  });
 });
 
 app.post("/campgrounds", function(req, res) {
   //getting data
   var name = req.body.name;
   var image = req.body.image;
-  var newCampground = {name: name, image: image};
-  campgrounds.push(newCampground);
+  var description = req.body.description;
+  var newCampground = {name: name, image: image, description: description};
+  // campgrounds.push(newCampground);
+  Campground.create(newCampground, function(err, newCreated) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(newCreated)
+      res.redirect("/campgrounds");
+    }
+  });
+});
 
-  res.redirect("/campgrounds");
+app.get("/campgrounds/new", function(req, res) {
+  res.render("new");
+});
+
+// shows more info about a specific campground
+app.get("/campgrounds/:id", function(req, res) {
+  Campground.findById(req.params.id, function(err, foundCampground) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("show", {campground:foundCampground});
+    }
+  });
 });
 
 app.listen(3000, process.env.PORT, process.env.IP, function () {
