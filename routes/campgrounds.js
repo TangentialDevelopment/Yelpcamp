f672760c51adfabfeef41dc09c2f9be5e0c5fd6a
TangentialDevelopment <joetang148@gmail.com>
2018-09-10 17:02:13 -0400

    update and destroy routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -50,6 +50,39 @@ router.get("/:id", function(req, res) {
   });
 });
 
+// Edit
+router.get("/:id/edit", function(req, res) {
+  Campground.findById(req.params.id, function(err, foundCampground) {
+    if (err) {
+      res.redirect("/campgoreunds");
+    } else {
+      res.render("campgrounds/edit", {campground: foundCampground});
+    }
+  });
+});
+
+// Update
+router.put("/:id", function(req, res) {
+  Campground.findOneAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground) {
+    if (err) {
+      res.redirect("/campgrounds");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
+});
+
+// Destroy
+router.delete("/:id", function(req, res) {
+  Campground.findOneAndDelete(req.params.id, function(err) {
+    if (err) {
+      res.redirect("/campgrounds");
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
+});
+
 // middleware
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
