d81ec83daa523f3ac696596685f028e77010aeaf
TangentialDevelopment <joetang148@gmail.com>
2018-09-20 17:04:19 -0400

    using enivromental variables

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes       = require("./routes/index");
 
-// mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
-mongoose.connect("mongodb://test:test123@ds111063.mlab.com:11063/testyelpcamp", {useNewUrlParser: true});
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + "/public"));
